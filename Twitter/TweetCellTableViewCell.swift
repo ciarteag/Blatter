aa46371602c9ef83f79ef3be7d4836711df531a3
Carlos Arteaga <ciarteag@uci.edu>
2020-04-26 13:33:48 -0700

    Added the ability to login, logout, and load tweets.

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Carlos Arteaga on 4/25/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
